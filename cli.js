e7f99c89fe47d53cbbcb49df8c9e4145fb016051
Yves Richard <yves.richard@tesera.com>
2017-02-01 16:33:38 -0700

    Update catalog structure

--- cli.js
+++ cli.js
@@ -1,7 +1,20 @@
 'use strict';
+var program = require('commander');
 var getCalatog = require('./');
 
-const url = process.argv[2];
-getCalatog(url, function (err, catalog) {
-    console.log(JSON.stringify(catalog));
+let endpointUrl;
+
+program
+  .version('0.0.1')
+  .arguments('<url>')
+  .option('--include-layers', 'Should it harvest layer level metadata?', false)
+  .action(function (url) {
+     endpointUrl = url;
+  });
+
+program.parse(process.argv);
+
+getCalatog(endpointUrl, { includeLayers: program.includeLayers } , function (err, catalog) {
+    if (err) console.log(err)
+    else console.log(JSON.stringify(catalog));
 });
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 'use strict';
+let wait = require('co-wait');
 let co = require('co');
 let request = require('co-request');
 
+let delay = 0;
 function* get (url) {
+    delay += 500;
+    yield wait(delay);
     const res = yield request(url, { qs: { f: 'json' } });
     return JSON.parse(res.body);
 }
 
-function getCatalog(url, callback) {
+function getCatalog(url, options, callback) {
     co(function* () {
         let index = {};
         
@@ -19,68 +23,48 @@ function getCatalog(url, callback) {
         const foldersMeta = yield folderUrls.map(url => get(url));
         folderUrls.forEach((f, i) => index[f] = foldersMeta[i] );
 
+        delay = 0;
+
         // INDEX SERVICES
         const serviceUrls = folderUrls.reduce((urls, folder) => 
             urls.concat(index[folder].services.map(s => url + `/${s.name}/${s.type}`)), []);
         const servicesMeta = yield serviceUrls.map(url => get(url));
         serviceUrls.forEach((url, i) => index[url] = servicesMeta[i] );
 
-        // INDEX LAYERS
-        const layerUrls = serviceUrls.reduce((layerUrls, serviceUrl) => {
-            let urls = [];
-            if (index[serviceUrl].layers)
-                urls = index[serviceUrl].layers.map(l => serviceUrl + `/${l.id}`);
-            return layerUrls.concat(urls);
-        }, []);
-        const layersMeta = yield layerUrls.map(url => get(url));
-        layerUrls.forEach((l, i) => index[l] = layersMeta[i] );
+        delay = 0;
 
         const servicesByFolder = folderUrls.reduce((byFolder, folderUrl) => {
             byFolder[folderUrl] = serviceUrls.filter(url => url.startsWith(folderUrl));
             return byFolder;
         }, {});
 
-        const layersByService = serviceUrls.reduce((byService, serviceUrl) => {
-            byService[serviceUrl] = layerUrls.filter(url => url.startsWith(serviceUrl));
-            return byService;
-        }, {});
+        const catalog = {
+            folderServices: servicesByFolder,
+            folders: folderUrls,
+            services: serviceUrls,
+            metadata: index
+        };
 
-        const getLayerNamebyUrl = (url) => {
-            const layerId = url.substring(url.lastIndexOf('/') + 1);
-            const serviceUrl = url.substring(0, url.lastIndexOf('/'));
-            const service = index[serviceUrl];
-            const layer = service.layers.filter((l) => l.id == layerId);
-            return layer[0] && layer[0].name ? layer[0].name : '';
+        // INDEX LAYERS
+        if(options.includeLayers) {
+            const layerUrls = serviceUrls.reduce((layerUrls, serviceUrl) => {
+                let urls = [];
+                if (index[serviceUrl].layers)
+                    urls = index[serviceUrl].layers.map(l => serviceUrl + `/${l.id}`);
+                return layerUrls.concat(urls || []);
+            }, []);
+            const layersMeta = yield layerUrls.map(url => get(url));
+            layerUrls.forEach((l, i) => index[l] = layersMeta[i] );
+
+            const layersByService = serviceUrls.reduce((byService, serviceUrl) => {
+                byService[serviceUrl] = layerUrls.filter(url => url.startsWith(serviceUrl));
+                return byService;
+            }, {});
+
+            catalog.serviceLayers = layersByService;
+            catalog.index = index;
         }
 
-        const namesByUrl = Object.keys(index).reduce((byUrl, url) => {
-            const parts = url.split('/');
-            switch(parts.length) {
-                case 7:
-                    byUrl[url] = parts[6];
-                    return byUrl;
-                case 9:
-                    byUrl[url] = parts[7];
-                    return byUrl;
-                case 10:
-                    byUrl[url] = getLayerNamebyUrl(url);
-                    return byUrl;
-                default:
-                    return byUrl;
-            }
-            
-        }, {});
-        
-        const catalog = {
-            servicesByFolder: servicesByFolder,
-            layersByService: layersByService,
-            namesByUrl: namesByUrl,
-            folder: folderUrls,
-            service: serviceUrls,
-            layer: layerUrls,
-            metaByUrl: index,
-        };
-        
         callback(null, catalog);
     }).catch(callback);
 }
